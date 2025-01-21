cd2eb24c4c7ede113bceddc71c752b6e18498e46
kylian <kylianconraux@hotmail.fr>
2025-01-21 13:28:50 +0100

    ajout des check de forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,55 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent {
   title = 'Portfolio';
-  
+
+
   ngOnInit() {
     initFlowbite();
   }
+
+
+  checkMail(mail: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(mail);
+  }
+
+  checkObject(object: string): boolean {
+    const objectRegex = /^[a-zA-Z0-9\s.,!?-]{1,255}$/;
+    return objectRegex.test(object);
+  }
+
+  checkMessage(message: string): boolean {
+    const messageRegex = /^[a-zA-Z0-9\s.,!?@#$%&*()_+:'"=/\r\n-]{1,2000}$/;
+    return messageRegex.test(message);
+  }
+
+  highlightError(input: HTMLInputElement): void {
+    input.style.backgroundColor = 'lightcoral';
+    setTimeout(() => {
+      input.style.backgroundColor = "#f0eeee";
+    }, 1000);
+  }
+
+  checkForm(): boolean {
+    const mail = document.getElementById('email') as HTMLInputElement;
+    const object = document.getElementById('objet') as HTMLInputElement;
+    const message = document.getElementById('message') as HTMLInputElement;
+
+    let test = true;
+
+    if (!this.checkMail(mail.value)) {
+      this.highlightError(mail);
+      test = false;
+    }
+    if (!this.checkObject(object.value)) {
+      this.highlightError(object);
+      test = false;
+    }
+    if (!this.checkMessage(message.value)) {
+      this.highlightError(message);
+      test = false;
+    }
+    return test;
+  }
+
 }
