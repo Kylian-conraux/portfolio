d044587322586a65c57a77381640c374eaa16fa9
kylian <kylianconraux@hotmail.fr>
2025-01-23 11:20:58 +0100

    set up service with emailJS

--- src/app/service/email.service.ts
+++ src/app/service/email.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  private api_pubkey = "";
+  private service_id = "";
+  private template_id = "";
+
+  constructor() {
+    emailjs.init(this.api_pubkey);
+  }
+
+  sendEmail(formData: { email: string, objet: string, message: string }): Promise<EmailJSResponseStatus> {
+    return emailjs.send(this.service_id, this.template_id, formData);
+  }
 }
